7cf6ce9cc276b715fa6ddd2c36edb569830d3349
yaelen <yaelensardjo@gmail.com>
2026-01-17 17:29:08 +0100

    hashrouting added, fix for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-import './App.css'
-import {createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import TProjects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
-        element: <Layout/>,
-        children:[
-            {
-                path: '/',
-                element: <Home/>
-            },
-            {
-                path: '/projects',
-                element: <TProjects/>
-            },
-            {
-                path: '/contact',
-                element: <Contact/>
-            }
-        ]
-    }
-])
+        element: <Layout />,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/projects", element: <TProjects /> },
+            { path: "/contact", element: <Contact /> },
+        ],
+    },
+]);
+
 function App() {
-    return <RouterProvider router={router} />
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
