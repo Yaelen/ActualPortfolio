45201cc011a07df69f11a37aeeba4968cae63e84
yaelen <yaelensardjo@gmail.com>
2026-01-18 16:18:38 +0100

    Base name added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import './App.css'
-import {createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import TProjects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            element: <Layout />,
+            children: [
+                { path: "/", element: <Home /> },
+                { path: "/projects", element: <TProjects /> },
+                { path: "/contact", element: <Contact /> },
+            ],
+        },
+    ],
     {
-        element: <Layout/>,
-        children:[
-            {
-                path: '/',
-                element: <Home/>
-            },
-            {
-                path: '/projects',
-                element: <TProjects/>
-            },
-            {
-                path: '/contact',
-                element: <Contact/>
-            }
-        ]
+        basename: "/ActualPortfolio",
     }
-])
+);
+
 function App() {
-    return <RouterProvider router={router} />
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
